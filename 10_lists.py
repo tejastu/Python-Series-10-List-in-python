4ba984a56e949ad16270821f75d2330ef94b939a
Tejas Parab <62276215+tejastu@users.noreply.github.com>
2021-01-21 19:36:55 +0530

    Add files via upload

---
+++ 10_lists.py
@@ -0,0 +1,16 @@
+# Create a list using []
+a = [1, 2, 4, 56, 6]
+
+# Print the list using print() function
+print(a)
+
+# Access using index using a[0], a[1], a[2]
+print(a[2])
+
+# Change the value of list using
+a[0] = 68
+print(a)
+
+# We can create a list with items of different types
+c = [45, True,  "Tejas", 4.7]
+print(c)
